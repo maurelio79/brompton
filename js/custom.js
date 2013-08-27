5bde3957aeec3ab2ffe73445e81eaf82aa39c9bb
maurelio79 <maurelio@DPL101.(none)>
2013-08-27 14:32:07 +0200

    Restyling everything

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+$( document ).on( "pageinit", function() {
+	$( "#popupPanel" ).on({
+		popupbeforeposition: function() {
+		    var h = $( window ).height();
+
+		    $( "#popupPanel" ).css( "height", h-2 );
+		}
+	});
+});
